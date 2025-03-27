1d2ad90828b54057e73f41818d0800320ea23403
Reymundo Figueroam <reymundo_124@hotmail.com>
2025-03-27 01:40:07 -0600

    add: pagina de login

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// Esperar a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Seleccionamos el botón por su tipo o por una clase/ID específico
+  const loginButton = document.querySelector("button[type='submit']");
+
+  // Verificamos que el botón exista antes de agregar el evento
+  if (loginButton) {
+    loginButton.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevenir el envío del formulario
+
+      // Redirigir a la página home.html en la nueva ubicación
+      window.location.href = "../../public/pages/home.html";
+    });
+  }
+});
