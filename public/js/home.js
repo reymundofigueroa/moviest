a47cfbe19630a2824bd5ed9bbcf0c501659d5017
Reymundo Figueroam <reymundo_124@hotmail.com>
2025-04-02 00:49:58 -0600

    Add: funciones para renderizar por series, por peliculas y el home

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,48 @@
-const moviesContainer = document.getElementById('moviesContainer')
+const moviesListContainer = document.getElementById('moviesContainer')
+const homeBtn = document.getElementById('home-btn')
+const favoritesBtn = document.getElementById('favorites-btn')
+const moviesBtn = document.getElementById('movie-btn')
+const seriesBtn = document.getElementById('series-btn')
+let homeLoader;
+
+const genres = ["Acción", "Misterio", "Comedia", "Drama", "Terror", "Ciencia Ficción", "Suspenso", "Fantasía"]
+const homeCategories = ['movies', 'series']
+
 async function getMovieList(){
   try{
-    const response = await fetch('../js/data.json')
-    const data = await response.json()
-
-    renderMovieList(data.movies, moviesContainer)
+    moviesContainer.innerHTML = ''
+    renderMovieList(data.movies, moviesContainer, 'Películas')
     console.log(data)
   }
-  catch{
-    console.log('error')
+  catch(error){
+    console.log(error)
   }
 }
-getMovieList()
+
+async function getSeriesList(){
+  try{
+    moviesContainer.innerHTML = ''
+    renderMovieList(data.series, moviesContainer, 'Series')
+    console.log(data)
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+
+
+function getMoviesAndSeriesByCategory(array) {
+    return function(){
+      moviesContainer.innerHTML = '';
+      for (let content of array) {
+        renderMovieList(data[content], moviesContainer, content);
+      }
+    } 
+  };
+
+
+homeLoader = getMoviesAndSeriesByCategory(homeCategories)
+window.addEventListener('DOMContentLoaded', homeLoader)
+homeBtn.addEventListener('click', homeLoader)
+moviesBtn.addEventListener('click', getMovieList)
+seriesBtn.addEventListener('click', getSeriesList)
