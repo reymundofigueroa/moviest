8a42c2085fe87fda0bd8eb0505c9e8f013131913
Reymundo Figueroam <reymundo_124@hotmail.com>
2025-04-19 20:23:37 -0600

    Refact(styles): ajustes en los estilos del nav-menu

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { DataMovies, MoviesData } from '../shared/models/data-movies';
+import { MoviesService } from '../core/services/movies.service';
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
-export class SearchBarComponent {
+export class SearchBarComponent  {
 
 }
