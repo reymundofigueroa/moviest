4e8acfcbcca147e1ddfd977697b1a8a865ee924f
Reymundo Figueroa <reymundo_124@hotmail.com>
2025-04-27 21:06:45 -0600

    Test: Tests para el componente de movieDetails

--- src/app/features/movie/movie-details/movie-details.component.spec.ts
+++ src/app/features/movie/movie-details/movie-details.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MovieDetailsComponent } from './movie-details.component';
+import { FavoritesService } from '../services/favorites.service';
+import { ElementRef } from '@angular/core';
+
 
 describe('MovieDetailsComponent', () => {
   let component: MovieDetailsComponent;
@@ -17,7 +19,76 @@ describe('MovieDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debería crear el componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Debería crear un addEventListener', () => {
+    // Arrange
+    spyOn(document, 'addEventListener')
+
+    // Act
+    component.ngOnChanges()
+
+    // Assert
+    expect(document.addEventListener).toHaveBeenCalled()
+    expect(document.addEventListener).toHaveBeenCalledWith('fullscreenchange', jasmine.any(Function));
+  })
+
+  it('Debería eliminar el addEventListener', () => {
+    // arrange
+    spyOn(document, 'removeEventListener')
+
+    // Act
+    component.ngOnDestroy()
+
+    // Assert
+    expect(document.removeEventListener).toHaveBeenCalled()
+    expect(document.removeEventListener).toHaveBeenCalledWith('fullscreenchange', jasmine.any(Function))
+  })
+
+  it('Debería mostrar el video', () => {
+    // Arrange
+    component.videoContainerElement = {
+      nativeElement: {
+        style: { display: '' }
+      }
+    } as ElementRef<HTMLDivElement>;
+
+    component.videoElement = {
+      nativeElement: {
+        play: jasmine.createSpy('play'),
+        requestFullscreen: jasmine.createSpy('requestFullscreen')
+      } as unknown as HTMLVideoElement
+    } as ElementRef<HTMLVideoElement>;
+  // Act
+  component.showVideo();
+
+  // Assert
+  expect(component.videoContainerElement.nativeElement.style.display).toBe('block')
+  expect(component.videoElement.nativeElement.play).toHaveBeenCalled()
+  expect(component.videoElement.nativeElement.requestFullscreen).toHaveBeenCalled()
 });
+
+it('Debería pausar y ocultar la película', () => {
+  // Arrange
+  component.videoContainerElement = {
+    nativeElement: {
+      style: { display: '' }
+    }
+  } as ElementRef<HTMLDivElement>;
+
+  component.videoElement = {
+    nativeElement: {
+      pause: jasmine.createSpy('pause'),
+    } as unknown as HTMLVideoElement
+  } as ElementRef<HTMLVideoElement>;
+
+  // Act
+  component.handleFullscreenExit()
+
+  // Assert
+  expect(component.videoElement.nativeElement.pause).toHaveBeenCalled()
+  expect(component.videoContainerElement.nativeElement.style.display).toBe('none')
+})
+})
