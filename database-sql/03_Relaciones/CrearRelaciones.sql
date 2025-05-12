f84ccd5723258d9b7b5418305464acac5af80a58
Reymundo Figueroa <reymundo_124@hotmail.com>
2025-05-11 22:47:29 -0600

    Add: Procedimientos para guardar peliculas y usuarios

--- database-sql/03_Relaciones/CrearRelaciones.sql
+++ database-sql/03_Relaciones/CrearRelaciones.sql
@@ -3,17 +3,19 @@
 -- ====================================
 
 USE Moviest_DB
+GO
 
 ALTER TABLE Favoritos
 ADD CONSTRAINT FK_Favorites_Users
     FOREIGN KEY (UserId)
     REFERENCES Usuarios(id);
+GO
 
 ALTER TABLE Favoritos
 ADD CONSTRAINT FK_Favorites_Contents
     FOREIGN KEY (ContentId)
     REFERENCES Contenidos(id);
-
+GO
 
 -- ==================================
 -- Create Foreign Key Contenidos
@@ -23,3 +25,4 @@ ALTER TABLE Contenidos
 ADD CONSTRAINT FK_Contents_Categories
     FOREIGN KEY (Genre)
     REFERENCES Categorias(id);
+GO
---
+++ database-sql/04_Procedimientos/InsertarContenidoProc.sql
@@ -0,0 +1,25 @@
+USE Moviest_DB
+GO
+
+IF OBJECT_ID('dbo.InsertarContenidoPorc', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.InsertarContenidoPorc
+GO
+
+CREATE PROCEDURE dbo.InsertarContenidoPorc
+    @Title NVARCHAR(150),
+    @ContentDescription NVARCHAR(MAX) = NULL,
+    @Genre INT = NULL,
+    @ContentYear DATE = NULL,
+    @Rating DECIMAL(2,1) = NULL,
+    @Duration TIME = NULL,
+    @CoverImage NVARCHAR(300) = NULL,
+    @VideoUrl NVARCHAR(300) = NULL
+AS
+BEGIN
+    -- Evitamos el mensaje de cuantas filas fueron afectadas
+    SET NOCOUNT ON;
+
+    INSERT INTO Contenidos (Title, ContentDescription, Genre, ContentYear, Rating, Duration, CoverImage, VideoUrl)
+    VALUES (@Title, @ContentDescription, @Genre, @ContentYear, @Rating, @Duration, @CoverImage, @VideoUrl);
+END
+GO
