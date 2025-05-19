43ce9795532d7a2fe14e99c042e1aa75454f447e
Reymundo Figueroa <reymundo_124@hotmail.com>
2025-05-19 13:16:18 -0600

    Add: funcionalidad de JWT en la API

--- database-sql/02_CrearTablas/CrearListaFavoritos.sql
+++ database-sql/02_CrearTablas/CrearListaFavoritos.sql
@@ -4,13 +4,13 @@
 USE Moviest_DB
 GO
 
-IF OBJECT_ID('dbo.Favoritos', 'U') IS NOT NULL
+IF OBJECT_ID('dbo.Favorites', 'U') IS NOT NULL
   DROP TABLE dbo.Favorites
 GO
 
 CREATE TABLE dbo.Favorites
 (
-	UserId INT NOT NULL, 
-	ContentId INT NOT NULL, 
+	UserId INT NOT NULL,
+	ContentId INT NOT NULL,
 )
 GO
