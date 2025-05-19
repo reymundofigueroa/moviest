a0bfa26ec9b1a8abb4669f11083d882ae8064637
Reymundo Figueroa <reymundo_124@hotmail.com>
2025-05-19 16:41:52 -0600

    Add: consumo de la API para login y crear usuarios desde el frontend

--- apps/frontend/src/app/features/user-access/create-account/create-account.component.ts
+++ apps/frontend/src/app/features/user-access/create-account/create-account.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../layout/header/header.component";
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { UserAccessService } from '../services/user-access.service';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.css'
 })
 export class CreateAccountComponent {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: UserAccessService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      birthDate: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+
+    if (this.registerForm.invalid) {
+      console.log('formato invalido')
+      return;}
+
+    this.authService.register(this.registerForm.value).subscribe({
+      next: (res) => {
+        alert('Cuenta creada correctamente');
+        this.router.navigate(['/login']);
+        console.log('respuesta', res)
+      },
+      error: (err) => {
+        alert('Error: ' + (err.error || 'No se pudo crear el usuario'));
+        console.error(err);
+      }
+    });
+  }
 }
--- apps/frontend/src/app/features/user-access/login/login.component.ts
+++ apps/frontend/src/app/features/user-access/login/login.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../../layout/header/header.component';
+import { UserAccessService } from '../services/user-access.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email = '';
+  password = '';
+  errorMessage = '';
 
-  // Método para redirigir al Home
-  goHome(event: Event) {
+  constructor(private authService: UserAccessService, private router: Router) {}
+
+  login(event: Event) {
     event.preventDefault();
-    this.router.navigate(['/home']);
+
+    this.authService.login({ email: this.email, password: this.password }).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+        localStorage.setItem('token', response.token ?? '');
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        if (error.status === 401) {
+          this.errorMessage = 'Credenciales inválidas';
+        } else {
+          this.errorMessage = 'Ocurrió un error al intentar iniciar sesión.';
+        }
+      }
+    });
   }
 
   goToCreateAccount() {
@@ -25,4 +45,5 @@ export class LoginComponent {
   goToRecoverPassword() {
     this.router.navigate(['/forgot-password']);
   }
+
 }
