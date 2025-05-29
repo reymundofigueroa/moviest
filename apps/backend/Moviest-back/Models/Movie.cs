cd0778824d6c1bb5f3ffcc1ece83f6b93c2a5592
Reymundo Figueroa <reymundo_124@hotmail.com>
2025-05-29 14:08:06 -0600

    Add: controlador para las peticiones de peliculas en la API

--- apps/backend/Moviest-back/Models/Movie.cs
+++ apps/backend/Moviest-back/Models/Movie.cs
@@ -1,12 +1,26 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Moviest_back.Models
 {
   public class Movie
   {
-    public int Id { get; set; }
+    public int id { get; set; }
+
     public string Title { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public DateTime ReleaseDate { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public int CategoryId { get; set; }
+
+    public string? ContentDescription { get; set; }
+
+    public int Genre { get; set; }
+
+    public DateTime? ContentYear { get; set; }
+
+    [Column(TypeName = "decimal(2,1)")]
+    public decimal? Rating { get; set; }
+
+    public TimeSpan? Duration { get; set; } // Alternativa a TimeOnly
+
+    public string? CoverImage { get; set; }
+
+    public string? VideoUrl { get; set; }
   }
 }
